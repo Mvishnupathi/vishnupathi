681518c8e8aac96bb7823d57e1caf14c14647449
Mvishnupathi <107763341+Mvishnupathi@users.noreply.github.com>
2022-06-28 17:34:16 +0530

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,16 @@
+package org.system;
+
+public class Desktop extends computer {
+	public void desktopSize() {
+	}
+	
+	public static void main(String[] args) {
+		
+		Desktop size = new Desktop();
+		computer model = new computer();
+		size.desktopSize();
+		model.computerModel();
+		
+	
+	}
+}
