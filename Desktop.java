ad657b1f643aa4e6446ce882668c225a4f538026
Mvishnupathi <107763341+Mvishnupathi@users.noreply.github.com>
2022-06-28 19:57:43 +0530

    Update Desktop.java

--- Desktop.java
+++ Desktop.java
@@ -2,8 +2,9 @@ package org.system;
 
 public class Desktop extends computer {
 	public void desktopSize() {
+	     
+		system.out.println("This desktop size is 28 inches");
 	}
-	
 	public static void main(String[] args) {
 		
 		Desktop size = new Desktop();
